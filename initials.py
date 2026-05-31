905ada2d990d5ea5b4de26ce7e9d70a3f27429f8
qhoby <amohkobina55@gmail.com>
2026-05-31 19:38:46 +0000

    completed assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials (PKA).
+# Assignment Number: 1
+# 
+# Name: Paa Kobina Amoh
+# STUDENT ID:  2425403434
+# Email: 2425403434@live.gctu.edu.gh
+# Grader: Augustus
+# 
+# On my honor, Paa Kobina Amoh this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print out my initials in stylized large letters.
+    print()
+    print("...PKA")
+    print()
+    print("...PPPPPPPPP...........KK........KK.............AA..........")
+    print("...PPPPPPPPPP..........KK......KK..............AAAA.........")
+    print("...PP......PP..........KK....KK...............AA..AA........")
+    print("...PP......PP..........KK..KK.................AA....AA.......")
+    print("...PPPPPPPPPP..........KKKK...................AA....AA.......")
+    print("...PPPPPPPPP...........KKKK...................AAAAAAAA.......")
+    print("...PP..................KK..KK.................AAAAAAAA.......")
+    print("...PP..................KK....KK...............AA....AA.......")
+    print("...PP.............**...KK......KK.....**.....AA....AA.....**")
+    print("...PP.............**...KK........KK...**.....AA....AA.....**")
+    print()
 
 
 main()
